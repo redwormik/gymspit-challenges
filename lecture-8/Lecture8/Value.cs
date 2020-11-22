6204e50e1103941e451968d98c35261ea3323486
Jakub Červenka <jakub.cervenka@netvor.co>
2020-11-22 22:42:28 +0100

    Add lecture 8 Iterator (Enumerator) project

---
+++ lecture-8/Lecture8/Value.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Lecture8
+{
+	class Value: Expression
+	{
+		private readonly int value;
+
+
+		public Value(int value)
+		{
+			this.value = value;
+		}
+
+		public int GetValue()
+		{
+			return value;
+		}
+
+
+		public IEnumerator<Expression> GetEnumerator()
+		{
+			yield return this;
+		}
+
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+	}
+}
